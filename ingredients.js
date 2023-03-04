41ad77820572fcf4346093d76385f4da669dd65c
Shilpa7866 <126277187+Shilpa7866@users.noreply.github.com>
2023-03-04 18:55:09 -0500

    Add files via upload
    Working with While loop, for loop, reverse . Iterating over arrays: Banana Bread

---
+++ ingredients.js
@@ -0,0 +1,16 @@
+const ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+console.log(ingredients);
+for (let i = 0; i <ingredients.length; i++) {
+  console.log(ingredients[i]);
+}
+
+
+let i = 0;
+while (i < ingredients.length) {
+  console.log(ingredients[i]);
+  i++;
+}
+
+const reversed = ingredients.reverse();
+console.log(reversed);
+
